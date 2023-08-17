027722bd985d4e0bbd293ad6f9f937817b7c1492
Dia Kharrat <dkharrat@gmail.com>
2023-08-17 00:06:15 -0700

    remove dircolors since it messes up terminals without solarized

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile bashrc vimrc vim dircolors gitconfig"    # list of files/folders to symlink in homedir
+files="bash_profile bashrc vimrc vim gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
